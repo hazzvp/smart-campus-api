6d83f10e75f84015b6c1f14058df13002d73fa8e
hazzvp <hasandivihanga@gmail.com>
2026-04-18 12:54:26 +0530

    create the DataStore.java class

--- CSA-CW/src/main/java/com/smartcampus/DataStore.java
+++ CSA-CW/src/main/java/com/smartcampus/DataStore.java
@@ -17,5 +17,31 @@ import java.util.Map;
  * @author Hasandi Vihanga
  */
 public class DataStore {
+    public static final Map<String, Room> rooms = new HashMap<>();
+    public static final Map<String, Sensor> sensor = new HashMap<>();
+    public static final Map<String, List<SensorReading>> readings = new HashMap<>();
     
+    static{
+        Room r1 = new Room("LIB-301", "Library Quiet Study", 50);
+        Room r2 = new Room("LAB-101", "Computer Science Lab", 30);
+        rooms.put(r1.getId(), r1);
+        rooms.put(r2.getId(), r2);
+        
+        Sensor s1 = new Sensor("TEMP-001", "Temperature", "Active", 22.5, "LIB-301");
+        Sensor s2 = new Sensor("CO2-001", "CO2", "Active", 400.0, "LAB-101");
+        sensor.put(s1.getId(), s1);
+        sensor.put(s2.getId(), s2);
+        
+        r1.getSensorIds().add(s1.getId());
+        r2.getSensorIds().add(s2.getId());
+        
+        List<SensorReading> r1Readings = new ArrayList<>();
+        r1Readings.add(new SensorReading(21.0));
+        r1Readings.add(new SensorReading(22.5));
+        readings.put(s1.getId(), r1Readings);
+        
+        List<SensorReading> r2Readings = new ArrayList<>();
+        r2Readings.add(new SensorReading(398.0));
+        readings.put(s2.getId(), r2Readings);
+    }
 }
